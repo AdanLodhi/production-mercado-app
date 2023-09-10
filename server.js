2ecaaa5180114c286e34af615f7accef7f76ae5c
Adan Lodhi <125998573+AdanLodhi@users.noreply.github.com>
2023-09-10 16:27:26 +0500

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use (express.static(path.join(__dirname, './client/build')))
+app.use (express.static(path.join(__dirname, './client/dist')))
 
 //Routes
 app.use("/api/v1/auth", authRoutes);
@@ -34,7 +34,7 @@ app.use("/api/v1/product", productRoutes);
 
 //Rest Api
 app.use('*', function(req, res){
- res.sendFile(path.join(__dirname, "./client/build/index.html"))
+ res.sendFile(path.join(__dirname, "./client/dist/index.html"))
 })
 
 //Port
